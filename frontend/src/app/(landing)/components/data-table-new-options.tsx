c95dc56c4b4b01a6ce5098620415045fa572fd8f
Nam Hoang <56421424+namvhoang02@users.noreply.github.com>
2024-09-12 11:31:32 +0700

    show-empty-state (#25)
    * feat: saved
    
    * feat: saved
    
    * feat: saved
    
    * feat: saved
    
    ---------
    
    Co-authored-by: Nam Hoang <particle4dev@gmail.com>

--- frontend/src/app/(landing)/components/data-table-new-options.tsx
+++ frontend/src/app/(landing)/components/data-table-new-options.tsx
@@ -1,205 +1,22 @@
 'use client';
 
-import {
-  Ed25519PublicKey,
-  InputGenerateTransactionPayloadData,
-} from '@aptos-labs/ts-sdk';
-import { useWallet } from '@aptos-labs/wallet-adapter-react';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { Plus } from 'lucide-react';
-import React, { useState } from 'react';
-import { type SubmitHandler, useForm } from 'react-hook-form';
-import { z } from 'zod';
+import React from 'react';
 
-import { getAptosClient } from '@/lib/aptosClient';
-import { getTxUrl } from '@/lib/chain';
-import { MODULE_ADDRESS } from '@/lib/constants';
-
-import { useLandingContext } from '@/components/landing/context/selectors';
 import { Button } from '@/components/ui/button';
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { useToast } from '@/components/ui/use-toast';
+import { DialogTrigger } from '@/components/ui/dialog';
 
-import formSchema from './form-schema';
-
-// Default values for the form
-const DEFAULT_VALUES = {
-  title: '',
-};
-
-type FormSchemaType = z.infer<typeof formSchema>;
+import { AddNewTaskModel } from './AddNewTaskModel';
 
 export function DataTableNewOptions() {
-  const { toast } = useToast();
-  const { state, addTask } = useLandingContext();
-  const [open, setOpen] = useState(false);
-  const { account, signAndSubmitTransaction, network } = useWallet();
-
-  const client = getAptosClient();
-
-  const form = useForm<FormSchemaType>({
-    resolver: zodResolver(formSchema),
-    mode: 'onChange',
-    criteriaMode: 'all',
-    defaultValues: DEFAULT_VALUES,
-  });
-
-  const {
-    handleSubmit,
-    reset,
-    formState: { isSubmitting, isValid },
-  } = form;
-
-  const handleError = (message: string) => {
-    toast({
-      title: 'Error',
-      description: message,
-      variant: 'destructive',
-    });
-  };
-
-  const onSubmit: SubmitHandler<FormSchemaType> = async (data) => {
-    if (!account?.address) {
-      handleError('No account address available.');
-      return;
-    }
-
-    try {
-      // Build transaction payload
-      const payload: InputGenerateTransactionPayloadData = {
-        function: `${MODULE_ADDRESS}::todolist::create_task`,
-        functionArguments: [data.title],
-      };
-
-      // Build raw transaction
-      const rawTxn = await client.transaction.build.simple({
-        sender: account.address,
-        data: payload,
-      });
-
-      // Simulate transaction to estimate gas
-      const publicKey = new Ed25519PublicKey(account.publicKey.toString());
-      const [simulationResult] = await client.transaction.simulate.simple({
-        signerPublicKey: publicKey,
-        transaction: rawTxn,
-        options: {
-          estimateGasUnitPrice: true,
-          estimateMaxGasAmount: true,
-          estimatePrioritizedGasUnitPrice: true,
-        },
-      });
-
-      if (!simulationResult) {
-        handleError('Failed to simulate transaction.');
-        return;
-      }
-
-      // Prepare transaction with gas estimates
-      const pendingTxn = await signAndSubmitTransaction({
-        data: payload,
-        options: {
-          maxGasAmount: Math.ceil(Number(simulationResult.gas_used) * 1.2),
-          gasUnitPrice: Number(simulationResult.gas_unit_price),
-        },
-      });
-
-      // Wait for transaction confirmation
-      const response = await client.waitForTransaction({
-        transactionHash: pendingTxn.hash,
-      });
-
-      if (response?.success) {
-        addTask &&
-          addTask({
-            id: `${state.list.length + 1}`,
-            title: data.title,
-            status: 'backlog',
-          });
-
-        reset(DEFAULT_VALUES);
-        setOpen(false);
-        toast({
-          title: 'Success',
-          description: (
-            <a target='_blank' href={getTxUrl(pendingTxn.hash, network?.name)}>
-              View on AptosScan
-            </a>
-          ),
-        });
-      } else {
-        handleError(`Transaction failed: ${response.vm_status}`);
-      }
-    } catch (error: any) {
-      handleError(`Transaction error: ${error.message}`);
-    }
-  };
-
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <AddNewTaskModel>
       <DialogTrigger asChild>
         <Button size='sm' className='ml-auto hidden h-8 lg:flex'>
           <Plus className='mr-2 h-4 w-4' />
           New
         </Button>
       </DialogTrigger>
-      <DialogContent className='sm:max-w-[425px]'>
-        <Form {...form}>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <DialogHeader>
-              <DialogTitle>Create a new task</DialogTitle>
-              <DialogDescription>
-                Create a new task to track your progress.
-              </DialogDescription>
-            </DialogHeader>
-
-            <FormField
-              control={form.control}
-              name='title'
-              render={({ field }) => (
-                <FormItem className='my-3'>
-                  <FormLabel>Title</FormLabel>
-                  <FormControl>
-                    <Input
-                      type='text'
-                      placeholder='Title'
-                      disabled={isSubmitting}
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage className='text-red-600 dark:text-red-500' />
-                </FormItem>
-              )}
-            />
-
-            <DialogFooter>
-              <Button
-                className='py-3 px-4 inline-flex justify-center items-center'
-                type='submit'
-                disabled={isSubmitting || !isValid}
-              >
-                {isSubmitting ? 'Submitting...' : 'Save changes'}
-              </Button>
-            </DialogFooter>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
+    </AddNewTaskModel>
   );
 }
--- frontend/src/app/(landing)/components/data-table.tsx
+++ frontend/src/app/(landing)/components/data-table.tsx
@@ -1,9 +1,5 @@
 'use client';
 
-import {
-  Ed25519PublicKey,
-  InputGenerateTransactionPayloadData,
-} from '@aptos-labs/ts-sdk';
 import { useWallet } from '@aptos-labs/wallet-adapter-react';
 import {
   ColumnDef,
@@ -19,10 +15,9 @@ import {
   useReactTable,
   VisibilityState,
 } from '@tanstack/react-table';
+import { Plus } from 'lucide-react';
 import React, { useMemo, useState } from 'react';
 
-import { getAptosClient } from '@/lib/aptosClient';
-import { MODULE_ADDRESS } from '@/lib/constants';
 import { HTTP_STATUS } from '@/lib/constants';
 import { useIsMounted } from '@/lib/hooks/useIsMounted';
 
@@ -42,15 +37,17 @@ import {
 import { WalletAdapterButton } from '@/components/wallet-adapter/WalletAdapterButton';
 import { WalletAdapterModelDialog } from '@/components/wallet-adapter/WalletAdapterModelDialog';
 
+import { AddNewTaskModel } from './AddNewTaskModel';
 import { DataTablePagination } from './data-table-pagination';
 import { DataTableToolbar } from './data-table-toolbar';
+import { useCreateTodoList } from './useCreateTodoList';
 
 interface DataTableProps<TData, TValue> {
   fetchStatus: string | null;
   data: TData[];
   columns: ColumnDef<TData, TValue>[];
   hasTodoList: boolean;
-  updateHasTodoList?: (hasTodoList: boolean) => void
+  updateHasTodoList?: (hasTodoList: boolean) => void;
 }
 
 export function DataTable<TData, TValue>({
@@ -97,81 +94,114 @@ export function DataTable<TData, TValue>({
     [fetchStatus],
   );
 
-  const { account, signAndSubmitTransaction } = useWallet();
+  const { createList, loading } = useCreateTodoList(updateHasTodoList);
 
-  const client = getAptosClient();
+  // Renders skeleton row while data is being fetched or components are mounting
+  const renderSkeletonRow = () => (
+    <TableRow key='skeleton-row-key'>
+      {columns.map((column) => (
+        <TableCell key={column.id} className='text-center'>
+          {'accessorKey' in column && column.accessorKey === 'id' && (
+            <div className='min-h-[300px] text-center flex flex-col items-center justify-center'>
+              <Skeleton className='h-4 w-[50px]' />
+            </div>
+          )}
+          {'accessorKey' in column && column.accessorKey === 'title' && (
+            <Skeleton className='h-4 w-[150px]' />
+          )}
+          {'accessorKey' in column && column.accessorKey === 'status' && (
+            <Skeleton className='h-4 w-[100px]' />
+          )}
+          {column.id === 'actions' && <Skeleton className='h-4 w-[50px]' />}
+        </TableCell>
+      ))}
+    </TableRow>
+  );
 
-  const createList = async () => {
-    try {
-      if (!account) {
-        return;
-      }
-      // build transaction
-      const payload: InputGenerateTransactionPayloadData = {
-        function: `${MODULE_ADDRESS}::todolist::create_list`,
-        functionArguments: [],
-      };
-      const rawTxn = await client.transaction.build.simple({
-        sender: account.address,
-        data: payload,
-      });
+  // Renders the message when no wallet is connected
+  const renderNoWalletConnected = () => (
+    <TableRow>
+      <TableCell colSpan={columns.length}>
+        <div className='min-h-[300px] text-center flex flex-col items-center justify-center'>
+          <p className='text-lg font-semibold'>Wallet not connected</p>
+          <p className='text-sm text-muted-foreground'>
+            Please connect your wallet to proceed.
+          </p>
+          <WalletAdapterModelDialog>
+            <DialogTrigger asChild>
+              <WalletAdapterButton icon className='mt-2' />
+            </DialogTrigger>
+          </WalletAdapterModelDialog>
+        </div>
+      </TableCell>
+    </TableRow>
+  );
 
-      const publicKey = new Ed25519PublicKey(account.publicKey.toString());
-      const userTransaction = await client.transaction.simulate.simple({
-        signerPublicKey: publicKey,
-        transaction: rawTxn,
-        options: {
-          estimateGasUnitPrice: true,
-          estimateMaxGasAmount: true,
-          estimatePrioritizedGasUnitPrice: true,
-        },
-      });
+  // Renders the message when there is no data
+  const renderNoData = () => (
+    <TableRow>
+      <TableCell colSpan={columns.length}>
+        <div className='min-h-[300px] text-center flex flex-col items-center justify-center'>
+          <p className='text-lg font-semibold'>No data found</p>
+          <p className='text-sm text-muted-foreground'>
+            It looks like there's nothing here yet.
+          </p>
 
-      const pendingTxn = await signAndSubmitTransaction({
-        data: payload,
-        options: {
-          maxGasAmount: parseInt(
-            String(Number(userTransaction[0].gas_used) * 1.2),
-          ),
-          gasUnitPrice: Number(userTransaction[0].gas_unit_price),
-        },
-      });
+          <AddNewTaskModel>
+            <DialogTrigger asChild>
+              <Button className='mt-2'>
+                <Plus className='mr-2 h-4 w-4' /> Create new task
+              </Button>
+            </DialogTrigger>
+          </AddNewTaskModel>
+        </div>
+      </TableCell>
+    </TableRow>
+  );
 
-      const response = await client.waitForTransaction({
-        transactionHash: pendingTxn.hash,
-      });
-      if (response && response?.success) {
-        console.log({ hash: pendingTxn?.hash, result: response });
-        updateHasTodoList && updateHasTodoList(true);
-      } else {
-        console.log({ message: response.vm_status || 'Transaction error!' });
-      }
-    } catch (error: any) {
-      console.log(error);
+  // Renders prompt to create a to-do list when none exists
+  const renderCreateListPrompt = () => (
+    <TableRow>
+      <TableCell colSpan={columns.length}>
+        <div className='min-h-[300px] text-center flex flex-col items-center justify-center'>
+          <p className='text-lg font-semibold'>No list available</p>
+          <p className='text-sm text-muted-foreground'>
+            Create a new list to get started.
+          </p>
+          <NetworksChecker>
+            <Button onClick={createList} disabled={loading} className='mt-2'>
+              {loading ? 'Creating...' : 'Create your list'}
+            </Button>
+          </NetworksChecker>
+        </div>
+      </TableCell>
+    </TableRow>
+  );
+
+  // Main table content renderer with conditional display logic
+  const renderTableContent = () => {
+    if (!isMounted) return renderSkeletonRow();
+    if (!connected && !isLoading) return renderNoWalletConnected();
+    if (connected && (isFetching || isLoading)) return renderSkeletonRow();
+    if (connected && hasTodoList) {
+      return table.getRowModel().rows.length > 0
+        ? table.getRowModel().rows.map((row) => (
+            <TableRow
+              key={row.id}
+              data-state={row.getIsSelected() && 'selected'}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))
+        : renderNoData();
+    } else {
+      return renderCreateListPrompt();
     }
   };
-
-  const renderSkeletonRow = useMemo(
-    () => (
-      <TableRow key='skeleton-row-key'>
-        {columns.map((column) => (
-          <TableCell key={column.id} className='h-24 text-center'>
-            {'accessorKey' in column && column.accessorKey === 'id' && (
-              <Skeleton className='h-4 w-[50px]' />
-            )}
-            {'accessorKey' in column && column.accessorKey === 'title' && (
-              <Skeleton className='h-4 w-[150px]' />
-            )}
-            {'accessorKey' in column && column.accessorKey === 'status' && (
-              <Skeleton className='h-4 w-[100px]' />
-            )}
-            {column.id === 'actions' && <Skeleton className='h-4 w-[50px]' />}
-          </TableCell>
-        ))}
-      </TableRow>
-    ),
-    [columns],
-  );
 
   return (
     <div className='space-y-4'>
@@ -193,78 +223,11 @@ export function DataTable<TData, TValue>({
               </TableRow>
             ))}
           </TableHeader>
-          <TableBody>
-            {/** Case 1: Not mounted, render skeleton */}
-            {!isMounted && renderSkeletonRow}
-
-            {/** Case 2: Mounted, not connected, not loading */}
-            {isMounted && !connected && !isLoading && (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className='h-24 text-center'
-                >
-                  No connected wallet.
-                  <br />
-                  <WalletAdapterModelDialog>
-                    <DialogTrigger asChild>
-                      <WalletAdapterButton icon />
-                    </DialogTrigger>
-                  </WalletAdapterModelDialog>
-                </TableCell>
-              </TableRow>
-            )}
-
-            {/** Case 3: Mounted, fetching data or wallet is loading */}
-            {isMounted &&
-              ((connected && isFetching) || isLoading) &&
-              renderSkeletonRow}
-
-            {/** Case 4: Mounted, connected, and has data */}
-            {isMounted &&
-              connected &&
-              !isFetching &&
-              (hasTodoList ? (
-                table.getRowModel().rows.map((row) => (
-                  <TableRow
-                    key={row.id}
-                    data-state={row.getIsSelected() && 'selected'}
-                  >
-                    {row.getVisibleCells().map((cell) => (
-                      <TableCell key={cell.id}>
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext(),
-                        )}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell
-                    colSpan={columns.length}
-                    className='h-24 text-center'
-                  >
-                    No results.
-                    <br />
-                    <NetworksChecker>
-                      <Button onClick={createList}>Create your list</Button>
-                    </NetworksChecker>
-                  </TableCell>
-                </TableRow>
-              ))}
-
-            {/** Case 5: Mounted, connected, fetch data */}
-            {isMounted && connected && (
-              <>
-                <FetchListData />
-              </>
-            )}
-          </TableBody>
+          <TableBody>{renderTableContent()}</TableBody>
         </Table>
       </div>
       <DataTablePagination table={table} />
+      {isMounted && connected && <FetchListData />}
     </div>
   );
 }
