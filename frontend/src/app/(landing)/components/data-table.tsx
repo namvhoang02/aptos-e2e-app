40bde7043548adb3dcdde5db2cb7c24a99377a8b
Nam Hoang <particle4dev@gmail.com>
2024-09-12 10:37:00 +0700

    feat: saved

--- frontend/src/app/(landing)/components/data-table.tsx
+++ frontend/src/app/(landing)/components/data-table.tsx
@@ -1,9 +1,5 @@
 'use client';
 
-import {
-  Ed25519PublicKey,
-  InputGenerateTransactionPayloadData,
-} from '@aptos-labs/ts-sdk';
 import { useWallet } from '@aptos-labs/wallet-adapter-react';
 import {
   ColumnDef,
@@ -19,10 +15,10 @@ import {
   useReactTable,
   VisibilityState,
 } from '@tanstack/react-table';
+import { Plus } from 'lucide-react';
 import React, { useMemo, useState } from 'react';
 
 import { getAptosClient } from '@/lib/aptosClient';
-import { MODULE_ADDRESS } from '@/lib/constants';
 import { HTTP_STATUS } from '@/lib/constants';
 import { useIsMounted } from '@/lib/hooks/useIsMounted';
 
@@ -44,13 +40,14 @@ import { WalletAdapterModelDialog } from '@/components/wallet-adapter/WalletAdap
 
 import { DataTablePagination } from './data-table-pagination';
 import { DataTableToolbar } from './data-table-toolbar';
+import { useCreateTodoList } from './useCreateTodoList';
 
 interface DataTableProps<TData, TValue> {
   fetchStatus: string | null;
   data: TData[];
   columns: ColumnDef<TData, TValue>[];
   hasTodoList: boolean;
-  updateHasTodoList?: (hasTodoList: boolean) => void
+  updateHasTodoList?: (hasTodoList: boolean) => void;
 }
 
 export function DataTable<TData, TValue>({
@@ -98,80 +95,93 @@ export function DataTable<TData, TValue>({
   );
 
   const { account, signAndSubmitTransaction } = useWallet();
-
   const client = getAptosClient();
+  const { createList, loading } = useCreateTodoList(updateHasTodoList);
 
-  const createList = async () => {
-    try {
-      if (!account) {
-        return;
-      }
-      // build transaction
-      const payload: InputGenerateTransactionPayloadData = {
-        function: `${MODULE_ADDRESS}::todolist::create_list`,
-        functionArguments: [],
-      };
-      const rawTxn = await client.transaction.build.simple({
-        sender: account.address,
-        data: payload,
-      });
+  const renderSkeletonRow = () => (
+    <TableRow key='skeleton-row-key'>
+      {columns.map((column) => (
+        <TableCell key={column.id} className='h-24 text-center'>
+          {'accessorKey' in column && column.accessorKey === 'id' && (
+            <Skeleton className='h-4 w-[50px]' />
+          )}
+          {'accessorKey' in column && column.accessorKey === 'title' && (
+            <Skeleton className='h-4 w-[150px]' />
+          )}
+          {'accessorKey' in column && column.accessorKey === 'status' && (
+            <Skeleton className='h-4 w-[100px]' />
+          )}
+          {column.id === 'actions' && <Skeleton className='h-4 w-[50px]' />}
+        </TableCell>
+      ))}
+    </TableRow>
+  );
 
-      const publicKey = new Ed25519PublicKey(account.publicKey.toString());
-      const userTransaction = await client.transaction.simulate.simple({
-        signerPublicKey: publicKey,
-        transaction: rawTxn,
-        options: {
-          estimateGasUnitPrice: true,
-          estimateMaxGasAmount: true,
-          estimatePrioritizedGasUnitPrice: true,
-        },
-      });
+  const renderNoWalletConnected = () => (
+    <TableRow>
+      <TableCell colSpan={columns.length} className='h-24 text-center'>
+        No connected wallet.
+        <br />
+        <WalletAdapterModelDialog>
+          <DialogTrigger asChild>
+            <WalletAdapterButton icon />
+          </DialogTrigger>
+        </WalletAdapterModelDialog>
+      </TableCell>
+    </TableRow>
+  );
 
-      const pendingTxn = await signAndSubmitTransaction({
-        data: payload,
-        options: {
-          maxGasAmount: parseInt(
-            String(Number(userTransaction[0].gas_used) * 1.2),
-          ),
-          gasUnitPrice: Number(userTransaction[0].gas_unit_price),
-        },
-      });
+  const renderNoData = () => (
+    <TableRow>
+      <TableCell colSpan={columns.length}>
+        <div className='min-h-[400px] text-center justify-center items-center flex-col flex'>
+          <p className='text-lg font-semibold'>No data</p>
+          <p className='text-sm text-muted-foreground'>No data here yet.</p>
+          <Button variant='secondary' className='mt-2'>
+            <Plus className='mr-2 h-4 w-4' /> Create new task
+          </Button>
+        </div>
+      </TableCell>
+    </TableRow>
+  );
 
-      const response = await client.waitForTransaction({
-        transactionHash: pendingTxn.hash,
-      });
-      if (response && response?.success) {
-        console.log({ hash: pendingTxn?.hash, result: response });
-        updateHasTodoList && updateHasTodoList(true);
-      } else {
-        console.log({ message: response.vm_status || 'Transaction error!' });
-      }
-    } catch (error: any) {
-      console.log(error);
+  const renderCreateListPrompt = () => (
+    <TableRow>
+      <TableCell colSpan={columns.length} className='h-24 text-center'>
+        No results.
+        <br />
+        <NetworksChecker>
+          <Button onClick={createList} disabled={loading}>
+            {loading ? 'Creating...' : 'Create your list'}
+          </Button>
+        </NetworksChecker>
+      </TableCell>
+    </TableRow>
+  );
+
+  const renderTableContent = () => {
+    if (!isMounted) return renderSkeletonRow();
+    if (!connected && !isLoading) return renderNoWalletConnected();
+    if (connected && (isFetching || isLoading)) return renderSkeletonRow();
+    if (connected && hasTodoList) {
+      return table.getRowModel().rows.length > 0
+        ? table.getRowModel().rows.map((row) => (
+            <TableRow
+              key={row.id}
+              data-state={row.getIsSelected() && 'selected'}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))
+        : renderNoData();
+    } else {
+      return renderCreateListPrompt();
     }
   };
-
-  const renderSkeletonRow = useMemo(
-    () => (
-      <TableRow key='skeleton-row-key'>
-        {columns.map((column) => (
-          <TableCell key={column.id} className='h-24 text-center'>
-            {'accessorKey' in column && column.accessorKey === 'id' && (
-              <Skeleton className='h-4 w-[50px]' />
-            )}
-            {'accessorKey' in column && column.accessorKey === 'title' && (
-              <Skeleton className='h-4 w-[150px]' />
-            )}
-            {'accessorKey' in column && column.accessorKey === 'status' && (
-              <Skeleton className='h-4 w-[100px]' />
-            )}
-            {column.id === 'actions' && <Skeleton className='h-4 w-[50px]' />}
-          </TableCell>
-        ))}
-      </TableRow>
-    ),
-    [columns],
-  );
 
   return (
     <div className='space-y-4'>
@@ -193,78 +203,11 @@ export function DataTable<TData, TValue>({
               </TableRow>
             ))}
           </TableHeader>
-          <TableBody>
-            {/** Case 1: Not mounted, render skeleton */}
-            {!isMounted && renderSkeletonRow}
-
-            {/** Case 2: Mounted, not connected, not loading */}
-            {isMounted && !connected && !isLoading && (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className='h-24 text-center'
-                >
-                  No connected wallet.
-                  <br />
-                  <WalletAdapterModelDialog>
-                    <DialogTrigger asChild>
-                      <WalletAdapterButton icon />
-                    </DialogTrigger>
-                  </WalletAdapterModelDialog>
-                </TableCell>
-              </TableRow>
-            )}
-
-            {/** Case 3: Mounted, fetching data or wallet is loading */}
-            {isMounted &&
-              ((connected && isFetching) || isLoading) &&
-              renderSkeletonRow}
-
-            {/** Case 4: Mounted, connected, and has data */}
-            {isMounted &&
-              connected &&
-              !isFetching &&
-              (hasTodoList ? (
-                table.getRowModel().rows.map((row) => (
-                  <TableRow
-                    key={row.id}
-                    data-state={row.getIsSelected() && 'selected'}
-                  >
-                    {row.getVisibleCells().map((cell) => (
-                      <TableCell key={cell.id}>
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext(),
-                        )}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell
-                    colSpan={columns.length}
-                    className='h-24 text-center'
-                  >
-                    No results.
-                    <br />
-                    <NetworksChecker>
-                      <Button onClick={createList}>Create your list</Button>
-                    </NetworksChecker>
-                  </TableCell>
-                </TableRow>
-              ))}
-
-            {/** Case 5: Mounted, connected, fetch data */}
-            {isMounted && connected && (
-              <>
-                <FetchListData />
-              </>
-            )}
-          </TableBody>
+          <TableBody>{renderTableContent()}</TableBody>
         </Table>
       </div>
       <DataTablePagination table={table} />
+      {isMounted && connected && <FetchListData />}
     </div>
   );
 }
